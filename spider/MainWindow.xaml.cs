f50c3656840f328d8b256abc852f0905cbe359dc
jxiong <xiongjianbang@126.com>
2016-12-30 16:56:50 +0800

    修正UI界面假死的问题

--- spider/MainWindow.xaml.cs
+++ spider/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace spider
     {
         public bool flag = true;
         delegate void BindDataAsync();
+        string url;
+        string selectType;
 
         public MainWindow()
         {
@@ -44,6 +46,8 @@ namespace spider
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            url = UrlBox.Text.ToString();
+            selectType = (string)comboBox.SelectedValue;
             BeginBindData();
         }
 
@@ -52,7 +56,7 @@ namespace spider
         {
             BindDataAsync bindData = new BindDataAsync(BindData);
             IAsyncResult iar = bindData.BeginInvoke(new AsyncCallback(EndBindData), bindData);
-            tsStatus.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            tsStatus.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate
             {
                 tsStatus.Content = "正在加载中。。。";
             }));
@@ -60,7 +64,7 @@ namespace spider
 
         private void EndBindData(IAsyncResult iar)
         {
-            tsStatus.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            tsStatus.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate
             {
                 tsStatus.Content = "加载完毕。。。";
             }));
@@ -70,14 +74,129 @@ namespace spider
 
         private void BindData()
         {
-            tsStatus.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+            tsStatus.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate
             {
                 tsStatus.Content = "正在绑定数据，请稍候。。。";
             }));
 
-            listViewBox.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+
+            List<Movie> movieList = new List<Movie>();
+            
+            if (url == String.Empty)
             {
-                getMovieData();
+                MessageBox.Show("请填写URL地址");
+                return;
+            }
+            try
+            {
+                Uri u = new Uri(url);
+                Movie newitem;
+                switch (selectType)
+                {
+                    case "single":
+                        switch (u.Host)
+                        {
+                            case "www.btba.com.cn":
+                                newitem = MovieDetailInfo.getBtbaMovieInfo(url);
+                                movieList.Add(newitem);
+                                break;
+                        }
+                        break;
+                    case "list":
+                        string strAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre";
+                        HttpWebResponse res = HttpHelper.CreateGetHttpResponse(url, 300, strAgent, null);
+                        if (res == null)
+                        {
+                            MessageBox.Show("URL无法访问");
+                            return;
+                        }
+                        string html = HttpHelper.GetResponseString(res);
+
+                        int nextPage = 1;
+                        HtmlNode nextNode;
+                        switch (u.Host)
+                        {
+                            case "www.btba.com.cn":
+                                HtmlDocument doc = new HtmlDocument();
+                                do
+                                {
+                                    //打开列表页
+                                    doc.LoadHtml(html);
+                                    HtmlNodeCollection collection = doc.DocumentNode.SelectNodes("//div[@class='left']/ul/li");
+                                    foreach (HtmlNode child in collection)
+                                    {
+                                        HtmlNode linode = HtmlNode.CreateNode(child.OuterHtml);
+                                        //获取单页详情地址
+                                        string sourceUrl = linode.SelectSingleNode("//a[@class='a']").Attributes["href"].Value;
+                                        newitem = MovieDetailInfo.getBtbaMovieInfo(sourceUrl);
+                                        //foreach (System.Reflection.PropertyInfo p in newitem.GetType().GetProperties())
+                                        //{
+                                        //    Console.WriteLine("Name:{0} Value:{1}", p.Name, p.GetValue(newitem));
+                                        //}
+                                        if (newitem != null)
+                                        {
+                                            movieList.Add(newitem);
+                                        }
+
+                                    }
+                                    //获取当前分页值的节点
+                                    HtmlNode node = doc.DocumentNode.SelectSingleNode("//div[@id='page']/b");
+                                    if (node != null)
+                                    {
+                                        int currentPage = Convert.ToInt32(node.InnerText);
+                                        //获取下一个兄弟元素 因为有一个换行符的文本节点，因此要两次，跳过换行那个文本节点
+                                        nextNode = node.NextSibling;
+                                        //没有下一页的节点了，就跳出循环
+                                        if (nextNode == null)
+                                        {
+                                            break;
+                                        }
+                                        nextNode = nextNode.NextSibling;
+                                        //下一个页码
+                                        nextPage = Convert.ToInt32(nextNode.InnerText);
+                                        //if (nextPage == 4)
+                                        //{
+                                        //    break;
+                                        //}
+                                    }
+                                    int index = url.IndexOf("?");
+                                    string newUrl = url;
+                                    if (index > 0)
+                                    {
+                                        newUrl = url.Substring(0, index);
+                                    }
+                                    Console.WriteLine(newUrl);
+                                    string nextUrl = newUrl + "?page=" + nextPage.ToString();
+                                    Console.WriteLine(nextUrl);
+                                    res = HttpHelper.CreateGetHttpResponse(nextUrl, 300, strAgent, null);
+                                    if (res == null)
+                                    {
+                                        continue;
+                                    }
+                                    html = HttpHelper.GetResponseString(res);
+                                    if (html == String.Empty)
+                                    {
+                                        continue;
+                                    }
+                                } while (true);
+
+                                break;
+                            default:
+                                MessageBox.Show("暂时不支持采集该站点");
+                                break;
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            listViewBox.Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(delegate
+            {
+                //数据统一装进来
+                listViewBox.ItemsSource = movieList;
             }));
         }
 
